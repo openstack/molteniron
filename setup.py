8c4ff32c15714093f4cd6fea1926cb177af003eb
Andreas Jaeger <aj@suse.com>
2020-02-04 22:27:25 +0100

    [ussuri][goal] Drop python 2.7 support and testing
    OpenStack is dropping the py2.7 support in ussuri cycle.
    
    Complete discussion & schedule can be found in
    - http://lists.openstack.org/pipermail/openstack-discuss/2019-October/010142.html
    - https://etherpad.openstack.org/p/drop-python2-support
    
    Ussuri Communtiy-wide goal:
    https://governance.openstack.org/tc/goals/selected/ussuri/drop-py27.html
    
    Use modern sphinx-build command.
    
    Change-Id: I8b6d7f69bb465150115cfc18a047e5775f1ff567

--- setup.py
+++ setup.py
@@ -17,14 +17,6 @@
 from setuptools import setup
 # from setuptools import find_packages
 
-# In python < 2.7.4, a lazy loading of package `pbr` will break
-# setuptools if some other modules registered functions in `atexit`.
-# solution from: http://bugs.python.org/issue15881#msg170215
-try:
-    import multiprocessing  # noqa
-except ImportError:
-    pass
-
 setup(name="molteniron",
       version="1.0",
       description="Client/server database for Ironic Baremetal services",
